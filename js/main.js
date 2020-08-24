cf4ed528d872e751ec65c88135f7098ebbe5571e
JUN-work <65006712+JUN-work@users.noreply.github.com>
2020-08-24 11:45:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -69,15 +69,3 @@ $(function(){
     animation();
   });
 });
-
-
-
-  // $(window).on("load", function() {
-
-  //   //ローディング
-  //   setTimeout(function() {
-  //     $('.loader').fadeOut();
-  //   },600)
-  
-  // });
-
